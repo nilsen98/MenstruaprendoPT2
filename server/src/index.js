412c223357763637fa071b988ce53fd26a8da9fa
Nilsen Alfaro <nilsen.alfaro@outlook.com>
2026-03-09 18:05:18 -0600

    Log incoming requests

--- server/src/index.js
+++ server/src/index.js
@@ -14,11 +14,6 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use((req, _res, next) => {
-  console.log('REQ =>', req.method, req.originalUrl);
-  next();
-});
-
 
 app.get('/', (_req, res) => res.send('Backend MenstruAprendo'));
 
